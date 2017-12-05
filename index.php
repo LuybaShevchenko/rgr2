3a45e650b79f3434e4713cb6ec3e8479270dd6d3
LuybaShevchenko <Luyba.S@yandex.ru>
2017-12-05 12:01:47 +0700

    rgr2

--- index.php
+++ index.php
@@ -3,29 +3,31 @@
 echo "<h1 style='text-align:center;'>Расчет площади стен в прямоугольной комнате</h1>";
 $Photo = htmlspecialchars('room.png');
 if (isset($_GET['height'])) {
-	$height = $_GET['height'];
+	$height = str_replace(",",".",$_GET['height']);
 } else {
 	$height = '';
 }
 if (isset($_GET['width'])) {
-	$width = $_GET['width'];
+	$width = str_replace(",",".",$_GET['width']);
 } else {
 	$width = '';
 }
 if (isset($_GET['length'])) {
-	$length = $_GET['length'];
+	$length =str_replace(",",".",$_GET['length']);
 } else {
 	$length = '';
 }
 
 $count = "";
+
 if (isset($_GET['button'])) {
-	if ( (!is_numeric($height))||(!is_numeric($length))||(!is_numeric($width)) ) {
+	if ((!is_numeric($height))||(!is_numeric($length))||(!is_numeric($width)) ) {
 		echo "<p><font color= 'red' size ='6' >Данные введены не корректно!</font></p>";
 	}else{
-		$count= $_GET['height']*$_GET['length']*2 + $_GET['width']*$_GET['height']*2;
+		$count= $height*$length*2 + $width*$height*2;
 	}
 }
+
 ?>
 	<body>
 		<table>
@@ -35,7 +37,7 @@ if (isset($_GET['button'])) {
 						<br> Высота (H): <input type="text" name="height" value="<?= htmlspecialchars($height) ?>"/> м
 						<br><br> Ширина(A): <input type="text" name="width" value="<?= htmlspecialchars($width) ?>"/> м
 						<br><br> Длина   (B):   <input type="text" name="length" value="<?= htmlspecialchars($length) ?>"/> м
-						<br><br> <input type="submit" name="button" value="Подсчитать"/>
+						<br><br> <input type="submit"  name="button" value="Подсчитать" />
 					</form>
 				</td>
 				<td>
@@ -44,7 +46,7 @@ if (isset($_GET['button'])) {
 			</tr>
 				<tr>
 					<td>
-						<br><br>Площадь стен =   <input type="text" name="square" value="<?php echo $count;?>"/> м<sup>2</sup>
+						<br><br>Площадь стен =   <input type="text" disabled name="square" value="<?php echo $count;?>"/> м<sup>2</sup>
 					</td>
 				</tr>
 		</table>
